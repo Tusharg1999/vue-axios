e737ece7a2551d951ac3116312fc58bfd8b78b50
Tusharg1999 <tushargoyal414@gmail.com>
2019-06-30 23:26:02 +0530

    using get functions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import App from './App.vue'
+import App from './components/LatestMovie'
 import Vue from 'vue'
+import './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import {router} from './router'
+import Vuetify  from 'vuetify'
+Vue.use(Vuetify)
 Vue.use(axios,VueAxios)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
